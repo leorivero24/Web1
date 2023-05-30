9db248924db450f33d1f36bd62e96af1d25c35ee
leorivero24 <88672669+leorivero24@users.noreply.github.com>
2023-05-30 09:35:36 -0300

    Commit on paginaContacto to contact Us whit js and css

--- paginaContacto/js/script.js
+++ paginaContacto/js/script.js
@@ -28,3 +28,110 @@ window.onscroll = function(){
     }
 
 }
+
+// FORMULARIO JS
+
+function submitForm() {
+    // Reinicia los mensajes de error
+    clearErrors();
+
+    // Validación de campos
+    if (validateForm()) {
+        showSubmittedData();
+    }
+}
+
+function clearErrors() {
+    const errorElements = document.getElementsByClassName('error');
+    Array.from(errorElements).forEach(function (element) {
+        element.textContent = '';
+    });
+}
+
+function validateForm() {
+    let isValid = true;
+
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const messageInput = document.getElementById('message');
+
+    if (!nameInput.value.trim()) {
+        isValid = false;
+        document.getElementById('nameError').textContent = 'Campo nombre es obligatorio.';
+    } else if (nameInput.value.length > 50) {
+        isValid = false;
+        document.getElementById('nameError').textContent = 'El campo nombre no puede tener más de 50 caracteres.';
+    }
+
+    if (!emailInput.value.trim()) {
+        isValid = false;
+        document.getElementById('emailError').textContent = 'Campo email es obligatorio.';
+    } else if (!isValidEmail(emailInput.value)) {
+        isValid = false;
+        document.getElementById('emailError').textContent = 'El campo email no es válido.';
+    }
+
+    if (!phoneInput.value.trim()) {
+        isValid = false;
+        document.getElementById('phoneError').textContent = 'Campo teléfono es obligatorio.';
+    } else if (!isValidPhone(phoneInput.value)) {
+        isValid = false;
+        document.getElementById('phoneError').textContent = 'El campo teléfono debe tener 10 dígitos numéricos.';
+    }
+
+    if (!messageInput.value.trim()) {
+        isValid = false;
+        document.getElementById('messageError').textContent = 'Campo mensaje es obligatorio.';
+    } else if (messageInput.value.length > 50) {
+        isValid = false;
+        document.getElementById('messageError').textContent = 'El campo mensaje no puede tener más de 50 caracteres.';
+    }
+
+    return isValid;
+}
+
+function isValidEmail(email) {
+    // Expresión regular para validar el formato de email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPhone(phone) {
+    // Expresión regular para validar el formato de teléfono (10 dígitos numéricos)
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phone);
+}
+
+function showSubmittedData() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    const dataDiv = document.createElement('div');
+
+    const nameParagraph = document.createElement('p');
+    nameParagraph.textContent = 'Nombre: ' + name;
+    dataDiv.appendChild(nameParagraph);
+
+    const emailParagraph = document.createElement('p');
+    emailParagraph.textContent = 'Email: ' + email;
+    dataDiv.appendChild(emailParagraph);
+
+    const phoneParagraph = document.createElement('p');
+    phoneParagraph.textContent = 'Teléfono: ' + phone;
+    dataDiv.appendChild(phoneParagraph);
+
+    const messageParagraph = document.createElement('p');
+    messageParagraph.textContent = 'Mensaje: ' + message;
+    dataDiv.appendChild(messageParagraph);
+
+    const submittedData = document.getElementById('submittedData');
+    submittedData.innerHTML = ''; // Limpia los datos previos
+    submittedData.appendChild(dataDiv);
+
+    document.getElementById('form').reset();
+    
+
+}
